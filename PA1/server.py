1205cc86f85ae6c62e415ecb8751ec62622cd7e5
Adam Sumilong <adamsumilong@lawn-143-215-122-152.lawn.gatech.edu>
2023-02-02 20:25:01 -0500

    server.py code brain dump, currently WIP

--- PA1/server.py
+++ PA1/server.py
@@ -3,14 +3,28 @@ import threading
 import sys
 import argparse
 
+
+def clientCode(connection):
+    pass
 def main():
     #python3 server.py -start -port <port> -passcode <passcode>
     parser = argparse.ArgumentParser()
     parser.add_argument('-start', nargs='?', const='', required=True)
-    parser.add_argument('-port', required=True)
+    parser.add_argument('-port', required=True, type=str)
     parser.add_argument('-passcode', required=True)
     args = parser.parse_args()
+
+    host = '127.0.0.1'
+    port = args.port
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind((host, port))
     print("Server started on port {}. Accepting connections".format(args.port))
+    sock.listen(25) #25 is arbitrary value
+
+    while True:
+        connection, address = sock.accept()
+
 
 if __name__ == "__main__":
 	main()
